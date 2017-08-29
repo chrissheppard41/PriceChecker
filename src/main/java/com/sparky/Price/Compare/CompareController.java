ec52248a3525f3a5c1cdf52da5bb14090fed9316
Chris Sheppard <chris.sheppard@seopa.com>
2017-08-29 14:00:41 +0100

    Adding in the admin section code to test out how that is going to work

--- src/main/java/com/sparky/Price/Compare/CompareController.java
+++ src/main/java/com/sparky/Price/Compare/CompareController.java
@@ -1,9 +1,7 @@
 package com.sparky.Price.Compare;
 
-import com.sparky.Price.Product.ProductController;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
