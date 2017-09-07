418cb744e8bb4f5b1cc522e242058e918fed9ab5
Chris Sheppard <chrissheppard41@gmail.com>
2017-09-07 19:15:53 +0100

    Reorganised the structure of the classes making them smaller

--- src/main/java/com/sparky/Price/Provider/model/Provider.java
+++ src/main/java/com/sparky/Price/Provider/model/Provider.java
@@ -5,15 +5,22 @@ import com.sparky.Price.Price.model.Price;
 import com.sparky.Price.Website.model.Website;
 import lombok.*;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by chrissheppard on 06/09/2017.
@@ -40,4 +47,55 @@ public class Provider {
     private List<Website> website;
 
 
+
+
+    private String[] getTargetNames() {
+        return targetName.split(",");
+    }
+
+
+
+    public Float getBestWebsitePrice(String url) throws IOException {
+        List<Float> listOfPrices = this.getAllPrices(Jsoup.connect(url).get());
+
+        if(listOfPrices.size() != 0) {
+            return listOfPrices.stream().min(Comparator.comparing(Float::floatValue)).get();
+        } else {
+            return 0f;
+        }
+
+    }
+
+    private List<Float> getAllPrices(Document doc) {
+        List<Float> elementText = new ArrayList<>();
+
+        for(String searchFor : this.getTargetNames()) {
+            Elements element = doc.select(searchFor);
+            if(element != null && element.size() != 0) {
+
+                elementText = Stream.concat(elementText.stream(), element.stream()
+                        .map(item -> this.getElementBestPrice(item.text())))
+                        .collect(Collectors.toList());
+
+            }
+        }
+
+        return elementText;
+    }
+
+    private Float getElementBestPrice(String input) {
+        Float output = 0f;
+
+        Pattern pattern = Pattern.compile("\\d{1,3}[,\\.]?(\\d{1,2})?");
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()) {
+            Float value = Float.parseFloat(matcher.group(0));
+
+            if(output == 0f || output > value) {
+                output = value;
+            }
+        }
+
+        return output;
+    }
 }
--- src/main/java/com/sparky/Price/Website/model/Website.java
+++ src/main/java/com/sparky/Price/Website/model/Website.java
@@ -32,9 +32,6 @@ import java.util.stream.Stream;
 @Getter
 @Setter
 public class Website {
-//todo: clean up this method by moving some of the methods about
-//todo: make sure it handles the null pointers
-//todo: ???
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -54,16 +51,13 @@ public class Website {
     @OneToMany(mappedBy = "website", cascade = CascadeType.DETACH)
     private List<Price> priceList;
 
-    private String[] getTargetNames() {
-        return provider.getTargetName().split(",");
-    }
 
     public Optional<Price> getHtmlPrice() throws IOException {
         return Optional.ofNullable(this.setPrice());
     }
 
     private Price setPrice() throws IOException {
-        Float websitePrice = this.getHtmlElement();
+        Float websitePrice = provider.getBestWebsitePrice(url);
         Price p = new Price();
         if(websitePrice != 0f) {
             if(this.anyChangeInPrice(websitePrice)) {
@@ -83,44 +77,7 @@ public class Website {
         return true;
     }
 
-    private Float getHtmlElement() throws IOException {
-        List<Float> elementText = new ArrayList<>();
-        Document doc = Jsoup.connect(url).get();
 
-        for(String searchFor : this.getTargetNames()) {
-            Elements element = doc.select(searchFor);
-            if(element != null && element.size() != 0) {
-
-                elementText = Stream.concat(elementText.stream(), element.stream()
-                    .map(item -> this.getPrice(item.text())))
-                    .collect(Collectors.toList());
-
-            }
-        }
-
-        if(elementText.size() != 0) {
-            return elementText.stream().min(Comparator.comparing(Float::floatValue)).get();
-        } else {
-            return 0f;
-        }
-
-    }
-
-    private Float getPrice(String input) {
-        Float output = 0f;
-
-        Pattern pattern = Pattern.compile("\\d{1,3}[,\\.]?(\\d{1,2})?");
-        Matcher matcher = pattern.matcher(input);
-        while (matcher.find()) {
-            Float value = Float.parseFloat(matcher.group(0));
-
-            if(output == 0f || output > value) {
-                output = value;
-            }
-        }
-
-        return output;
-    }
 
     public String toHtml() {
         PriceValue priceValue = new PriceValue(priceList);
